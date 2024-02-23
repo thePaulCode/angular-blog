9e8504fa68840a67a1d66e5ba9c80aa81d2c78dc
Paulo Santos <santospaulor21@gmail.com>
2024-02-23 09:17:21 -0300

    content created

---
+++ src/app/data/data-fake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "BEM-VINDO A ERA DA ENERGIA LIMPA!",
+    "description": "Reduza até 90% da sua conta de energia!",
+    "photoCover": "https://energybrasilsolar.com.br/novo/wp-content/uploads/2021/05/vantagens-da-energia-solar.jpeg"
+  },
+  {
+    "id": "2",
+    "title": "COLÉGIO MÃE DE DEUS ADERE À ENERGIA SOLAR",
+    "description": "Colégio",
+    "photoCover": "https://i.ibb.co/K0XrrLM/DJI-0540.jpg"
+  },
+  {
+    "id": "3",
+    "title": "ENERGIA SOLAR AJUDA FAMÍLIA DE AGRICULTORES A REDUZIR A CONTA DE LUZ E AUMENTAR OS GANHOS NA SAFRA",
+    "description": "agro",
+    "photoCover": "https://aalok.com.br/blog/wp-content/uploads/2023/09/Design-sem-nome-2023-09-19T114248.711-1.png"
+  },
+  {
+    "id": "4",
+    "title": "10 RAZÕES PARA VOCÊ INSTALAR ENERGIA SOLAR",
+    "description": "$",
+    "photoCover": "https://www.aldo.com.br/blog/wp-content/uploads/2021/01/blog_aldosolar_Saiba-qual-o-preco-e-os-custos-de-instalacao-de-Painel-Solar.jpg"
+  }
+]
